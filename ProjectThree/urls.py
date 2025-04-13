108958fc1d1f65cff2191055dbd9e6a3b2ecd77f
stekl0 <greystekl0@gmail.com>
2025-04-14 00:23:55 +0500

    lab 9.2

--- ProjectThree/urls.py
+++ ProjectThree/urls.py
@@ -21,13 +21,14 @@ from django.contrib import admin
 from django.urls import path, include
 
 from ProjectThree import views
+from articles import views as article_views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", views.homepage, name="homepage"),
+    path("", article_views.article_list, name="homepage"),
     path("about/", views.about, name="about"),
-    path("articles/", include("articles.urls"), name="articles"),
-    path("accounts/", include("accounts.urls"), name="accounts"),
+    path("articles/", include("articles.urls")),
+    path("accounts/", include("accounts.urls")),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
