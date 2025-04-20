37a69efe4758f9fba6b8e5f3e66340a94031ca99
stekl0 <greystekl0@gmail.com>
2025-04-20 21:03:46 +0500

    lab10

--- articles/forms.py
+++ articles/forms.py
@@ -3,7 +3,17 @@ from django import forms
 from . import models
 
 
-class CreateArticle(forms.ModelForm):
+class ArticleForm(forms.ModelForm):
     class Meta:
         model = models.Article
         fields = ["title", "body", "thumbnail"]
+        labels = {
+            "title": "Title",
+            "body": "Text",
+            "thumbnail": "Thumbnail",
+        }
+        widgets = {
+            "title": forms.TextInput(attrs={'placeholder': "write article title", "class": "form-control mb-3"}),
+            "body": forms.Textarea(attrs={'placeholder': "write article text", "class": "form-control mb-3"}),
+            "thumbnail": forms.FileInput(attrs={"class": "form-control mb-3"}),
+        }
